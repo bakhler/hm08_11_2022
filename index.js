140ed92f2c5890a013758cbbbaebdaeb498056eb
bakhler <thehenridit@gmail.com>
2022-11-09 21:09:54 +0200

    Last commit, add 3 if.

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const number1 = -0;
+const number = -0;
 const right= 0;
 
-if (number1 >= right) {
+if (number >= right) {
   console.log("positve");
 } else {
   console.log("negative");
 }
 
+const number1 = 3;
+const number2 = 3;
+
+if (number1 > number2) {
+  console.log(number1 - number2);
+} else {
+  console.log(number1 * number2);
+}
+
+const number5 = 10;
+if (Number(number5 % 7 === 0) && Number(number5 % 3 === 0)) {
+  console.log("You win!");
+} else {
+  console.log("You lose");
+}
